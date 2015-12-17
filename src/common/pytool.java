5685426cb461b6d13dcafad748d910da8bd5a2d3
suncl <suncl@fenbi.com>
2015-12-17 15:50:34 +0800

    add

--- src/common/pytool.java
+++ src/common/pytool.java
@@ -1,17 +1,26 @@
 package common;
 public class pytool {
-	private static final String MONKEYRUNNER = "$ANDROID_HOME/monkeyrunner";
-	private static final String TAKESHOT = MONKEYRUNNER + " pysrc/takeshot.py ";
+	private static final String MONKEYRUNNER = " $ANDROID_HOME/monkeyrunner ";
+	private static final String TAKESHOT = " takeshot.py ";
 	
 	public static void takeshot(String filename) throws Exception{
+		String pypath = new pytool().getPypath();
 		try{
-			JavaShellUtil.execute(TAKESHOT + filename);
+			JavaShellUtil.execute(MONKEYRUNNER + pypath + filename);
 		}catch(Exception e){
 			throw e;
 		}
 	}
 	
+	private String getPypath(){
+		String path = this.getClass().getClassLoader().getResource(".").getPath();
+		String pypath = path.split("bin/")[0]+"pysrc/";
+		System.out.println(pypath);
+		return pypath;
+	}
 	public static void main(String args[]) throws Exception{
-		takeshot("./test.png");
+		takeshot("/home/test.png");
+		//pytool p = new pytool();
+		//p.getPypath();
 	}
 }
