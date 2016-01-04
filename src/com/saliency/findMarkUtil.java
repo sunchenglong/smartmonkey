50c14b213058c576930d341d2e7da5c76fc311d0
sunchenglong <suncl@ios.ac.cn>
2016-01-04 22:17:08 +0800

    'add'

--- src/com/saliency/findMarkUtil.java
+++ src/com/saliency/findMarkUtil.java
@@ -22,4 +22,27 @@ public static int[] findMarkUtil_random(Mat src, int k_num, int marker){
 	}
 	return result;
 }
+
+
+public static int[] findMarkUtil_kmeans(Mat src, int k_num, int marker){
+	Random r = new Random();
+	int rows = (int) src.rows();
+	int cols = (int) src.cols();
+	int row = 0;
+	int col = 0;
+	int result[] = new int[k_num*2];
+	int curl = 0;
+	while(k_num>0){
+		row = r.nextInt(rows);
+		col = r.nextInt(cols);
+		if((int) (src.get(row,col)[0]) == marker){
+			result[curl] = row;
+			result[curl+1] = col;
+			curl += 2;
+			k_num --;
+		}	
+	}
+	return result;
+}
+
 }
---
+++ src/com/saliency/kmeansTest.java
@@ -0,0 +1,16 @@
+package com.saliency;
+
+import org.opencv.core.Scalar;
+
+public class kmeansTest {
+	public static void main(String args[]){
+		Scalar colorTab[]={
+		       new Scalar(0, 0, 255),
+		       new Scalar(0,255,0),
+		       new Scalar(255,100,100),
+		       new Scalar(255,0,255),
+		       new Scalar(0,255,255)
+		};
+
+	}
+}
