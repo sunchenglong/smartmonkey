fe5e8cd93be81485c6474777ee3729aaf3e56d29
sunchenglong <suncl@ios.ac.cn>
2016-03-15 22:35:53 +0800

    add

--- src/common/test.java
+++ src/common/test.java
@@ -2,7 +2,7 @@ package common;
 
 import java.io.BufferedReader;  
 import java.io.InputStreamReader;  
- 
+
 public class test {  
        public static void main(String[] args){  
                try{  
